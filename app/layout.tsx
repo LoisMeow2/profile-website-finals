60a8a3c4b18b9eeabec849c74c74cc6ca9c22205
Lois Vera Cruz <lcveracruz@student.apc.edu.ph>
2026-02-21 17:54:48 +0000

    images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Nunito, Lilita_One } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import './globals.css'
+import '../frontend/styles/globals.css'
 
 const nunito = Nunito({
   subsets: ['latin'],
